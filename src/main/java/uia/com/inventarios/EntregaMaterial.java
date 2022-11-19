229d8a13211c5b0424164b7c9e7bf12faad3a0cc
Magdalena Arana <a2260967@correo.uia.mx>
2022-11-19 11:12:01 -0600

    primer commit se crean metodos revisarSolicitudMaterial,validacionCondiciones;enviaSolicitudRetiroMaterialy la clase EntregaMAterial

---
+++ src/main/java/uia/com/inventarios/EntregaMaterial.java
@@ -0,0 +1,10 @@
+package uia.com.inventarios;
+
+public class EntregaMaterial {
+    public EntregaMaterial() {
+    }
+    public void enviarSolicitudRetiroMaterial(){
+        System.out.println("Entregado");
+
+    }
+}
---
+++ src/main/java/uia/com/inventarios/SolicitudSalidaMaterial.java
@@ -0,0 +1,30 @@
+package uia.com.inventarios;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SolicitudSalidaMaterial extends InfoInventarioUIA {
+
+    public SolicitudSalidaMaterial(InfoInventarioUIA sm) throws IOException {
+        super();
+    }
+
+    public SolicitudSalidaMaterial(int id, String name, String categoria, String cantidad, String idPartida, String idSubpartida, String idCategoria)
+    {
+        super(id, name, categoria, cantidad, idPartida, idSubpartida, idCategoria);
+    }
+
+    public SolicitudSalidaMaterial(String s) {
+    }
+
+    public void serializa(String nombre) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(new File(nombre), this);
+    }
+    public void revisarSolicitudMaterial(){
+        System.out.println("revision");
+
+    }
+}
